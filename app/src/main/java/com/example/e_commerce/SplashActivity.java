430bfa0428138560ed1d1f9582becb4b80100e10
Raul Paulino <20461484+rfire12@users.noreply.github.com>
2020-11-09 21:33:38 -0400

    add splash screen

---
+++ app/src/main/java/com/example/e_commerce/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.e_commerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int DELAY = 2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide(); // Hide header
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, DELAY);
+
+        Toast.makeText(SplashActivity.this, "LOADING", Toast.LENGTH_SHORT).show();
+    }
+}
