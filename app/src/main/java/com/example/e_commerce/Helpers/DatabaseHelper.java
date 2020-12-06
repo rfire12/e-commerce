a0c351bd4909560c3ece34990ca83e6050305227
José Gabriel González <jgg032899@gmail.com>
2020-12-06 17:34:02 -0400

    Update DatabaseHelper.java

--- app/src/main/java/com/example/e_commerce/Helpers/DatabaseHelper.java
+++ app/src/main/java/com/example/e_commerce/Helpers/DatabaseHelper.java
@@ -21,11 +21,15 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         // Here goes table creation
+        sqLiteDatabase.execSQL("create table categories (_id integer primary key autoincrement, name varchar(256) not null unique, image text)");
+        sqLiteDatabase.execSQL("create table products (_id varchar(256) primary key, name varchar(256) not null, description text not null, price float not null, categoryId integer, image text not null, foreign key(categoryId) references categories(_id))");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         // Here goes table drop
+        sqLiteDatabase.execSQL("drop table if exists products");
+        sqLiteDatabase.execSQL("drop table if exists categories");
         onCreate(sqLiteDatabase);
     }
 }
