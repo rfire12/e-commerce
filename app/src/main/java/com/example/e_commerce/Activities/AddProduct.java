19e2d0f57ecca27a278c09f0386759a56640b3b7
Raul Paulino <20461484+rfire12@users.noreply.github.com>
2020-12-07 01:43:10 -0400

    add product

---
+++ app/src/main/java/com/example/e_commerce/Activities/AddProduct.java
@@ -0,0 +1,27 @@
+package com.example.e_commerce.Activities;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.e_commerce.R;
+
+public class AddProduct extends AppCompatActivity {
+
+    private String CategoryName;
+    private Button AddNewProductButton;
+    private EditText InputProductName, InputProductPrice;
+    private ImageView InputProductImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product);
+        AddNewProductButton = findViewById(R.id.save_product_btn);
+        InputProductImage = findViewById(R.id.product_image);
+        InputProductName = findViewById(R.id.product_name);
+        InputProductPrice = findViewById(R.id.product_price);
+    }
+}
