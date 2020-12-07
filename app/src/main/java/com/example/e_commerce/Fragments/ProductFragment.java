b5ff16d71eb6876e48bfa988434a045eaf1c5e40
Raul Paulino <20461484+rfire12@users.noreply.github.com>
2020-12-07 10:48:48 -0400

    add product and checkout fragment

---
+++ app/src/main/java/com/example/e_commerce/Fragments/ProductFragment.java
@@ -0,0 +1,21 @@
+package com.example.e_commerce.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.e_commerce.R;
+
+public class ProductFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.product_fragment, container, false);
+    }
+}
