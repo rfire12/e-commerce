3e937715f5168774262f6134b19b6eaad6775ef6
Raul Paulino <20461484+rfire12@users.noreply.github.com>
2020-12-07 10:30:20 -0400

    add drawer items fragments

--- app/src/main/java/com/example/e_commerce/Activities/MainActivity.java
+++ app/src/main/java/com/example/e_commerce/Activities/MainActivity.java
@@ -1,37 +1,47 @@
 package com.example.e_commerce.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import com.example.e_commerce.Fragments.CategoryListFragment;
+import com.example.e_commerce.Fragments.MainFragment;
+import com.example.e_commerce.Fragments.ProductListFragment;
+import com.example.e_commerce.Fragments.ProfileFragment;
 import com.example.e_commerce.R;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
+{
 
     private Toolbar toolbar;
     private DrawerLayout drawer;
+    private NavigationView navigationView;
+
+    private FragmentManager fragmentManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        loadDrawer();
+        loadMainFragment();
 
-        drawer = findViewById(R.id.drawer);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
-                R.string.nav_open, R.string.nav_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+
     }
 
     @Override
@@ -41,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    // It can also be called to close the drawer
     @Override
     public void onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.START)) {
@@ -48,5 +59,58 @@ public class MainActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+
+    public void loadDrawer() {
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.nav);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.nav_open, R.string.nav_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    public void loadMainFragment() {
+        fragmentManager = getSupportFragmentManager();
+        fragmentManager
+                .beginTransaction()
+                .add(R.id.frame_container, new MainFragment())
+                .commit();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        fragmentManager = getSupportFragmentManager();
+        Fragment newFragment = new MainFragment(); // Default to home fragment
+
+        switch ( item.getItemId() ) {
+            case R.id.category_drawer:
+                newFragment = new CategoryListFragment();
+                break;
+            case R.id.product_drawer:
+                newFragment = new ProductListFragment();
+                break;
+            case R.id.profile_drawer:
+                newFragment = new ProfileFragment();
+                break;
+            case R.id.logout_drawer:
+                System.out.println("Close session");
+                break;
+        }
+
+        fragmentManager
+                .beginTransaction()
+                .replace(R.id.frame_container, newFragment)
+                .commit();
+
+        // Close drawer after click on a drawer item
+        onBackPressed();
+
+        return false;
     }
 }
