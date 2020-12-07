fa20bce8c0e9cdd1bc0b211ff88184383a063c69
José Gabriel González <jgg032899@gmail.com>
2020-12-06 22:52:30 -0400

    Setting up categories image upload

--- app/src/main/java/com/example/e_commerce/AddCategory.java
+++ app/src/main/java/com/example/e_commerce/AddCategory.java
@@ -14,18 +14,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import retrofit2.Retrofit;
 
 import java.io.File;
+import java.util.UUID;
 
 public class AddCategory extends AppCompatActivity {
 
     Button image_select, save, cancel;
     ImageView imageCat;
     StorageReference mStorageRef;
+    Uri filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,26 +38,22 @@ public class AddCategory extends AppCompatActivity {
         setContentView(R.layout.activity_add_category);
         imageCat = findViewById(R.id.create_cat_img);
         image_select = findViewById(R.id.select_img_btn);
-        image_select.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType("image/*");
-                startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);
-            }
+        image_select.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("image/*");
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), 1);
         });
-        save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
+        save.setOnClickListener(v -> {
+            StorageReference ref = mStorageRef.child("images/" + UUID.randomUUID().toString());
+            ref.putFile(filePath).addOnSuccessListener(taskSnapshot -> {
+                taskSnapshot.getMetadata().getReference().getDownloadUrl().addOnSuccessListener(download -> {
+                    // Store image URL
+                });
+            });
         });
-        cancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        cancel.setOnClickListener(v -> {
 
-            }
         });
 
         mStorageRef = FirebaseStorage.getInstance().getReference();
@@ -65,29 +65,12 @@ public class AddCategory extends AppCompatActivity {
         try {
             if (resultCode == RESULT_OK) {
                 if (requestCode == 1) {
-                    Uri selectedImageUri = data.getData();
-                    final String path = getPathFromURI(selectedImageUri);
-                    if (path != null) {
-                        File f = new File(path);
-                        selectedImageUri = Uri.fromFile(f);
-                    }
-                    imageCat.setImageURI(selectedImageUri);
+                    filePath = data.getData();
+                    imageCat.setImageURI(filePath);
                 }
             }
         } catch (Exception e) {
             Log.e("FileSelectorActivity", "File select error", e);
         }
-    }
-
-    public String getPathFromURI(Uri contentUri) {
-        String res = null;
-        String[] proj = {MediaStore.Images.Media.DATA};
-        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
-        if (cursor.moveToFirst()) {
-            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            res = cursor.getString(column_index);
-        }
-        cursor.close();
-        return res;
     }
 }
